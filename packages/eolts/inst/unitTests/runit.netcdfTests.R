9e5ff2ad7ea2739121a4db3d989f426889dee89e
Gordon Maclean <maclean@users.noreply.github.com>
2013-11-10 14:58:35 -0700

    Add unit test for reading ISFS netcdf files with a station dimension.

--- packages/eolts/inst/unitTests/runit.netcdfTests.R
+++ packages/eolts/inst/unitTests/runit.netcdfTests.R
@@ -26,7 +26,7 @@ test.netcdf = function()
 
     checkEquals(dim(x),c(21,2))
 
-    # dput(x,file.path(datadir,"test1.Rdput",control="all"))
+    # dput(x,file.path("/tmp","test1.Rdput"),control="all")
     # test1.Rdput then needs editing to be successfully read by dget:
     #   remove ".Data =" in initialization of utimes (3 places)
 
@@ -42,6 +42,52 @@ test.netcdf = function()
     # checkEquals(positions(x),positions(xx))
     # cat(positions(x),"\n")
     # cat(positions(xx),"\n")
+
+}
+
+test.netcdf2 = function()
+{
+
+    wd <- getwd()
+    cat("wd=",wd,"\n")
+    pkg <- sub("\\.Rcheck$", '', basename(dirname(wd)))
+    cat("pkg=",pkg,"\n")
+
+    datadir <- file.path(system.file("unitTests", package = pkg),"data")
+    cat("datadir=",datadir,"\n")
+
+    ncfile  <- "test_%Y%m%d.nc"
+
+    t1  <- utime("2012 oct 10 19:00:00",time.zone="US/Mountain")
+    t2  <- utime("2012 oct 11 20:00:00",time.zone="US/Mountain")
+
+    con = netcdf(file=ncfile,dir=datadir,start=t1,end=t2)
+
+    on.exit(close(con))
+
+    # test read of variables by their netcdf name and their short_name
+    x = readts(con,c("u_1m","u_10m_M"), start=t1, end=t2)
+    cat("dim(x)=",dim(x),"\n")
+    checkEquals(dim(x),c(300,22))
+
+    x = readts(con,c("u.1m","u.10m.M"), start=t1, end=t2,stns=0:21)
+    cat("dim(x)=",dim(x),"\n")
+    checkEquals(dim(x),c(300,22))
+
+    # readts does not do any striding over the station dimension,
+    # returning all stations between the min and the max non-zero stations
+    # specified, so the stns sequence should increment by 1.
+    x = readts(con,c("u.1m","u_10m_M"), start=t1, end=t2,
+        stns=c(0,seq(from=3,length=5)))
+    cat("dim(x)=",dim(x),"\n")
+    checkEquals(dim(x),c(300,6))
+
+    # test2.Rdput then needs editing to be successfully read by dget:
+    #   remove ".Data =" in initialization of utimes (3 places)
+    dput(x,file.path("/tmp","test2.Rdput"),control="all")
+    xx = dget(file.path(datadir,"test2.Rdput"))
+    checkTrue(max(abs(x-xx),na.rm=TRUE) < 1.e-10)
+    checkTrue(all.equal(x@data,xx@data))
 
 }
 
