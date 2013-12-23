9c3d8d5d16ef76839de4278383297398a0992449
Gordon Maclean <maclean@users.noreply.github.com>
2013-12-22 22:47:37 -0700

    Remove browser.

--- packages/isfs/R/dat.R
+++ packages/isfs/R/dat.R
@@ -123,7 +123,7 @@ dat <- function(what,derived=T,cache=unlist(options("dcache")),
 
         # Look for a dat function  dat.what
         fcn <- paste("dat",what,sep=".")
-        if (fcn == "dat.heightSonic") browser()
+        # if (fcn == "dat.heightSonic") browser()
         if (existsFunction(fcn,generic=FALSE)) {
             f = getFunction(fcn,generic=FALSE,mustFind=TRUE,where=1)
             x = f(what=what,derived=derived,cache=cache,...)
