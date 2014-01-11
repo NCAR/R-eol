5e6ea71fd46f76fdcce029c86bfa182385185e66
Gordon Maclean <maclean@users.noreply.github.com>
2014-01-10 17:11:23 -0700

    Add time information below x axis label at mgp[1]+1.

--- packages/eolts/R/plot.spectra.R
+++ packages/eolts/R/plot.spectra.R
@@ -70,15 +70,18 @@ setMethod("plot","quadspectra",
 
 plot.spectra <- function(x,log,multfreq=T,falloff,
     grid=F,type="b",xlab,ylab,xlim,ylim,
-    xaxt,yaxt,tlwd=par("lwd"),col,logo=T,
-    ...) {
+    xaxt,yaxt,tlwd=par("lwd"),col,logo=T,mgp,
+    ...)
+{
 
     # tlwd: trace line width.  Use that option if you want to
     # 	change the trace line width without changing the line width
     #	of the axes.
 
-    # axt.old = par("xaxt","yaxt")
+    # axt.old <- par("xaxt","yaxt")
     # on.exit(par(axt.old))
+
+    args <- list(...)
 
     class.x <- class(x)
     # cat("class.x=",class.x,"\n")
@@ -91,39 +94,47 @@ plot.spectra <- function(x,log,multfreq=T,falloff,
     }
     else logarg <- log
 
-    if (missing(falloff)) falloff = logarg == "xy"
+    if (missing(falloff)) falloff <- logarg == "xy"
 
-    logx = any(substring(logarg,1:nchar(logarg),1:nchar(logarg)) == "x")
-    logy = any(substring(logarg,1:nchar(logarg),1:nchar(logarg)) == "y")
+    logx <- any(substring(logarg,1:nchar(logarg),1:nchar(logarg)) == "x")
+    logy <- any(substring(logarg,1:nchar(logarg),1:nchar(logarg)) == "y")
 
     if (par("xaxt") != "n") par(xaxt=ifelse(logx,"l","s"))
-    if (missing(xaxt)) xaxt = par("xaxt")
+    if (missing(xaxt)) xaxt <- par("xaxt")
     if (par("yaxt") != "n") par(yaxt=ifelse(logy,"l","s"))
-    if (missing(yaxt)) yaxt = par("yaxt")
+    if (missing(yaxt)) yaxt <- par("yaxt")
+
+    if (missing(mgp)) {
+        # hack: if mgp is the default 3,1,0, tighten it up
+        # to  c(1.5,0,0). We're adding an extra time label
+        # below the X axis (and I think c(3,1,0) is too much space).
+        mgp <- par("mgp")
+        if (identical(mgp,c(3,1,0))) mgp <- c(1.5,0,0)
+    }
+    cat("mgp=",paste(mgp,collapse=","),"\n")
 
     mfg <- par("mfg")
+
     nplotted <- (mfg[1] - 1) * mfg[4] + mfg[2]
     nplot <- mfg[3] * mfg[4]
     if (nplotted == nplot) nplotted <- 0
     # cat("nplotted=",nplotted," nplot=",nplot," mfg=",mfg,"\n")
     last.plot <- nplotted == nplot - 1
 
-    args = list(...)
-
-    nc = ncol(x)
-    dns = dimnames(x)[[2]]
+    nc <- ncol(x)
+    dns <- dimnames(x)[[2]]
 
     if (missing(multfreq) & class.x == "phase") {
         multfreq <- F
-        units = unique(x@units)
+        units <- unique(x@units)
         if (units[1] == "") x <- x*(180/pi)
-        x@units = rep("deg",ncol(x))
+        x@units <- rep("deg",ncol(x))
     }
     if (missing(xlab)) xlab <- paste("f (",x@funits,")",sep="")
 
-    units = unique(x@units)
+    units <- unique(x@units)
     if (length(units) > 1) {
-        units = paste(units,collapse=",")
+        units <- paste(units,collapse=",")
         warning(paste("more than one units to be plotted on one axis:",
                 units))
     }
@@ -132,8 +143,8 @@ plot.spectra <- function(x,log,multfreq=T,falloff,
         # If multiplying by frequency (1/s) then either remove trailing
         # " s" from units or add "/s".
         if (multfreq) {
-            sec = regexpr(" s$",units)
-            units = ifelse(sec < 0,paste(units,"/s"),substring(units,1,sec-1))
+            sec <- regexpr(" s$",units)
+            units <- ifelse(sec < 0,paste(units,"/s"),substring(units,1,sec-1))
         }
         if (nc == 1) {
             if (multfreq) ylab <- paste("f * S(",dns,") (",units,")",sep="")
@@ -152,9 +163,9 @@ plot.spectra <- function(x,log,multfreq=T,falloff,
         if (any(nonposf)) x@frequencies[nonposf] <- NA
     }
     if (missing(xlim))
-        frange = range(x@frequencies,na.rm=T)
+        frange <- range(x@frequencies,na.rm=T)
     else
-        frange = xlim
+        frange <- xlim
 
     if (multfreq) x <- x * x@frequencies
 
@@ -165,9 +176,9 @@ plot.spectra <- function(x,log,multfreq=T,falloff,
     }
 
     if (missing(ylim))
-        yrange = range(x@data,na.rm=T)
+        yrange <- range(x@data,na.rm=T)
     else
-        yrange = ylim
+        yrange <- ylim
 
     if (missing(col)) {
         if (nc > 1) col <- 2:(nc+1)
@@ -180,21 +191,21 @@ plot.spectra <- function(x,log,multfreq=T,falloff,
         plot(x@frequencies,x@data[,1],log=logarg,type="n",
             xlab=xlab,ylab=ylab,
             xaxt=xaxt,yaxt=yaxt,
-            xlim=frange,ylim=yrange,col=1,...)
+            xlim=frange,ylim=yrange,col=1,mgp=mgp,...)
     }
     else {
-        if (all(is.na(yrange))) yrange = c(.001,.01)  # fake it
-        ytmp = yrange[1] - .1 * (yrange[2] - yrange[1])
-        ytmp = rep(ytmp,length(x@frequencies))
+        if (all(is.na(yrange))) yrange <- c(.001,.01)  # fake it
+        ytmp <- yrange[1] - .1 * (yrange[2] - yrange[1])
+        ytmp <- rep(ytmp,length(x@frequencies))
         plot(x@frequencies,ytmp,log=logarg,type="n",
             xlab=xlab,ylab=ylab,
             xaxt=xaxt,yaxt=yaxt,
-            xlim=frange,ylim=yrange,col=1,err=-1,...)
+            xlim=frange,ylim=yrange,col=1,err=-1,mgp=mgp,...)
     }
 
-    # colv = 2:(nc+1)
-    legv = dns
-    ncolor = length(col)
+    # colv <- 2:(nc+1)
+    legv <- dns
+    ncolor <- length(col)
     for (ic in 1:nc) {
         par(new=T)
         if (any(!is.na(x@data[,ic]))) {
@@ -205,29 +216,32 @@ plot.spectra <- function(x,log,multfreq=T,falloff,
         }
     }
 
+    tlab <- paste(format(start(x),format="%Y %b %d %H:%M:%OS"),"-",format(end(x),format="%Y %b %d %H:%M:%OS %Z"))
+    mtext(tlab,side=1,line=mgp[1]+1,adj=0.5)
+
     lim <- par("usr")
 
     if ((is.numeric(falloff) || (is.logical(falloff) && falloff)) && logx && logy) {
-        if (frange[1] < 1 && frange[2] > 1) xm = 1
-        else xm = (frange[2] + frange[1]) / 2
+        if (frange[1] < 1 && frange[2] > 1) xm <- 1
+        else xm <- (frange[2] + frange[1]) / 2
 
-        fd = abs(x@frequencies - xm)
-        i = seq(along=x@frequencies)[!is.na(x@frequencies) &
+        fd <- abs(x@frequencies - xm)
+        i <- seq(along=x@frequencies)[!is.na(x@frequencies) &
             fd == min(fd,na.rm=T)][1]
 
-        xm = log10(x@frequencies[i])
-        ym = mean(log10(x@data[i,]),na.rm=T)
+        xm <- log10(x@frequencies[i])
+        ym <- mean(log10(x@data[i,]),na.rm=T)
 
-        if (is.numeric(falloff)) m = falloff
+        if (is.numeric(falloff)) m <- falloff
         else {
             if (any(class.x == "cospectra")) {
-                m = if(multfreq) -4/3 else -7/3
+                m <- if(multfreq) -4/3 else -7/3
             }
-            else m = if(multfreq) -2/3 else -5/3
+            else m <- if(multfreq) -2/3 else -5/3
             cat("falloff slope line=",signif(m,digits=3),"\n")
         }
         if (!is.na(ym)) {
-            b = ym - m * xm
+            b <- ym - m * xm
             if (!is.na(b) && !is.na(m)) abline(b,m,col=1,lty=2)
         }
     }
@@ -238,8 +252,8 @@ plot.spectra <- function(x,log,multfreq=T,falloff,
         else cex <- args$cex
         lx <- lim[1] + strheight("X") * 1.0
         ly <- lim[4] - strwidth("X") * 1.0
-        colv = col
-        if (length(colv) < nc) colv = rep(colv,(nc/length(colv))+1)
+        colv <- col
+        if (length(colv) < nc) colv <- rep(colv,(nc/length(colv))+1)
         # browser()
         horiz.legend(lx,ly,legv,col=colv,lty=rep(1,length(colv)),bty="n",cex=cex*1.0)
     }
@@ -250,7 +264,7 @@ plot.spectra <- function(x,log,multfreq=T,falloff,
 
     # lim <- par("usr")
     # cat("logarg=",logarg,"lim=",lim,"xaxt=",par("xaxt"),"yaxt=",par("yaxt"),"\n")
-    plot_spectra_tics(lim[1:2],lim[3:4],grid=grid,logx,logy,col=1)
+    plot_spectra_tics(lim[1:2],lim[3:4],grid=grid,logx,logy,col=1,mgp=mgp)
     if (logo && last.plot && exists("logo_stamp",mode="function")) {
         fls <- get("logo_stamp",mode="function")
         fls()
@@ -299,18 +313,18 @@ points.spectra <- function(x,xaxt=par("xaxt"),...)
     points(x@frequencies,x@data,xaxt=xaxt,...)
 }
 
-plot_spectra_tics <- function(xs,ys,grid,logx,logy,col=1)
+plot_spectra_tics <- function(xs,ys,grid,logx,logy,col=1,mgp)
 {
 
     if (logx) {
         l0 <- ceiling(xs[1])
         ln <- floor(xs[2])
-        pl = unique(trunc(pretty(c(l0,ln))))
-        pl = pl[pl>=l0]
-        pl = pl[pl<=ln]
+        pl <- unique(trunc(pretty(c(l0,ln))))
+        pl <- pl[pl>=l0]
+        pl <- pl[pl<=ln]
         axis(1,at=10^(l0:ln),labels=F,tck=.03,xaxt="s",yaxt="s",col=col)
         for (i in pl) 
-            mtext(substitute(10^i),side=1,line=par("mgp")[2]+0.2,at=10^i,adj=0.5)
+            mtext(substitute(10^i),side=1,line=mgp[2]+0.2,at=10^i,adj=0.5)
         axis(3,at=10^(l0:ln),labels=F,tck=.03,xaxt="s",yaxt="s",col=col)
 
         if (!missing(grid) && grid) abline(v=10^(l0:ln),lty=2,col=col)
@@ -335,13 +349,13 @@ plot_spectra_tics <- function(xs,ys,grid,logx,logy,col=1)
         if (logy) {
             l0 <- ceiling(ys[1])
             ln <- floor(ys[2])
-            pl = unique(trunc(pretty(c(l0,ln))))
-            pl = pl[pl>=l0]
-            pl = pl[pl<=ln]
+            pl <- unique(trunc(pretty(c(l0,ln))))
+            pl <- pl[pl>=l0]
+            pl <- pl[pl<=ln]
             #      axis(2,at=10^(l0:ln),labels=10^(l0:ln),tck=.03,srt=90,xaxt="s",yaxt="s",col=col)
             axis(2,at=10^(l0:ln),labels=F,tck=.03,srt=90,xaxt="s",yaxt="s",col=col)
             for (i in pl)
-                mtext(substitute(10^i),side=2,line=par("mgp")[2],at=10^i,adj=0.5,srt=90)
+                mtext(substitute(10^i),side=2,line=mgp[2],at=10^i,adj=0.5,srt=90)
             axis(4,at=10^(l0:ln),labels=F,tck=.03,xaxt="s",yaxt="s",col=col)
 
             if (!missing(grid) && grid) abline(h=10^(l0:ln),lty=2,col=col)
