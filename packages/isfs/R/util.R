0d5a9cf64036f39800bf6cfafe12a55eb0ac08cc
Gordon Maclean <maclean@users.noreply.github.com>
2014-01-14 17:18:43 -0700

    save/restore .projectEnv if sync() is run on project .RData

--- packages/isfs/R/util.R
+++ packages/isfs/R/util.R
@@ -2,6 +2,9 @@
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
 sync <- function(pos)
 {
+    # There may be an .projectEnv environment object in the
+    # $ISFF/projects/$PROJECT/ISFF/R/.RData. Save it before detaching/re-attaching
+    projectEnv <- NULL
     sl <- search()
     if (missing(pos)) {
         projdata <- paste(Sys.getenv("ISFF"),"projects",Sys.getenv("PROJECT"),
@@ -9,6 +12,7 @@ sync <- function(pos)
         pos <- grepl(projdata,sl)
         if (!any(pos)) stop(paste("pos is missing and can't find",projdata))
         pos <- base::seq(along=pos)[pos][1]
+        if (exists(".projectEnv",where=pos)) projectEnv <- get(".projectEnv",pos=pos)
     }
 
     rd <- sl[pos]
@@ -17,6 +21,7 @@ sync <- function(pos)
         rd <- sub("^file:","",rd)
         detach(pos=pos)
         attach(rd,pos=pos)
+        if (!is.null(projectEnv)) assign(".projectEnv",projectEnv,pos=pos)
     }
     else stop(paste("attached object at position",pos,"is not an .RData:",rd))
 
