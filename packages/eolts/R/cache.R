ce2932ef04daf4caec3944fd9d663fd4190b7674
Gordon Maclean <maclean@users.noreply.github.com>
2014-03-05 07:26:13 -0700

    added comments

--- packages/eolts/R/cache.R
+++ packages/eolts/R/cache.R
@@ -1,8 +1,20 @@
+# -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4; -*-
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
 #
 #               Copyright (C) by UCAR
 # 
 
-.eoltsEnv = new.env(parent=emptyenv())
+# Empty environment in the eolts namespace for adding session
+# cache variables. This is not in the global environment, and so is
+# not saved on exit. Currently it is not exported, and so
+# isn't in the package:eolts environment.
+# .getNamespace("eolts")
+# ls(all=TRUE,.getNamespace("eolts"))
+# get(".eoltsEnv",envir=.getNamespace("eolts"))
+# <environment: 0x54fec50>
+# eolts:::.eoltsEnv
+# <environment: 0x54fec50>
+.eoltsEnv <- new.env(parent=emptyenv())
 
 cache.name <- function(what)
 {
@@ -11,7 +23,7 @@ cache.name <- function(what)
 
 clear.cache <- function()
 {
-    cached = objects(pattern="^\\..*\\.cache$",envir=.eoltsEnv,all.names=TRUE)
+    cached <- objects(pattern="^\\..*\\.cache$",envir=.eoltsEnv,all.names=TRUE)
     # browser()
     if (length(cached) > 0)
         remove(list=cached,envir=.eoltsEnv)
@@ -19,7 +31,7 @@ clear.cache <- function()
 
 cache.object <- function(what,val)
 {
-    nm = cache.name(what)
+    nm <- cache.name(what)
     assign(nm,val,envir=.eoltsEnv);
 }
 
@@ -38,6 +50,6 @@ rm.cache.object <- function(what)
 
 check.cache <- function(what)
 {
-    nm = cache.name(what)
+    nm <- cache.name(what)
     exists(nm,envir=.eoltsEnv)
 }
