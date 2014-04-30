bda8211f4f4c9a645de69be04e87fdd7e41d5248
Gordon Maclean <maclean@users.noreply.github.com>
2014-04-30 16:03:51 -0600

    Query R to find .Library.site, rather than assume $(R RHOME)/site-library. Set the revision values of the packages to the number of git revisions.

--- packages/build.sh
+++ packages/build.sh
@@ -39,11 +39,15 @@ done
 # rm -rf /home/maclean/R/x86_64-redhat-linux-gnu-library/3.0/eolts
 # rm -rf /home/maclean/R/x86_64-redhat-linux-gnu-library/3.0/isfs
 
-rlib=$(R RHOME)/site-library
+rlib=$(R --vanilla --slave -e 'cat(.Library.site[1],"\n")' )
+# rlib=$(R RHOME)/site-library
 
 # --vanilla does --no-environ so we have to set R_LIBS_SITE ourselves
 # export R_LIBS_SITE=$rlib
 rargs="--vanilla"
+
+revision=$(git rev-list HEAD | wc -l)
+[ $revision -eq 0 ] && revision=1
 
 if $do_eolts; then
 
@@ -51,12 +55,24 @@ if $do_eolts; then
     autoreconf
     cd -
 
+    rm -f eolts_*.tar.gz
 
-    R $rargs CMD build eolts || exit ?
-    R $rargs CMD INSTALL -l $rlib eolts_[0-9].[0-9]-[0-9].tar.gz || exit $?
+    # make a backup of the eolts/DESCRIPTION file before changing the Version field
+    tmpdesc=$(mktemp /tmp/${0##*/}_XXXXXX)
+    cp eolts/DESCRIPTION $tmpdesc
+
+    sed -ri "s/^Version: *([0-9]+)\.([0-9]+)-.*/Version: \1.\2-$revision/" eolts/DESCRIPTION
+
+    R $rargs CMD build eolts
+    bstatus=$?
+    cp $tmpdesc eolts/DESCRIPTION
+    rm -f $tmpdesc
+    [ $bstatus -ne 0 ] && exit $bstatus
+
+    R $rargs CMD INSTALL -l $rlib eolts_[0-9].[0-9]-*.tar.gz || exit $?
 
     if $do_check; then
-        R $rargs CMD check -o /tmp eolts_[0-9].[0-9]-[0-9].tar.gz || exit $?
+        R $rargs CMD check -o /tmp eolts_[0-9].[0-9]-*.tar.gz || exit $?
         # R --vanilla --environ CMD check --use-valgrind -o /tmp eolts_*.tar.gz || exit $?
     fi
 fi
@@ -67,16 +83,30 @@ if $do_isfs; then
     autoreconf
     cd -
 
-    R $rargs CMD build isfs || exit ?
-    R $rargs CMD INSTALL -l $rlib isfs_[0-9].[0-9]-[0-9].tar.gz || exit $?
+    rm -f isfs_*.tar.gz
+
+    # make a backup of the isfs/DESCRIPTION file before changing the Version field
+    tmpdesc=$(mktemp /tmp/${0##*/}_XXXXXX)
+    cp isfs/DESCRIPTION $tmpdesc
+
+    sed -ri "s/^Version: *([0-9]+)\.([0-9]+)-.*/Version: \1.\2-$revision/" isfs/DESCRIPTION
+
+    R $rargs CMD build isfs
+    bstatus=$?
+    cp $tmpdesc isfs/DESCRIPTION
+    rm -f $tmpdesc
+    [ $bstatus -ne 0 ] && exit $bstatus
+
+
+    R $rargs CMD INSTALL -l $rlib isfs_[0-9].[0-9]-*.tar.gz || exit $?
 
     if $do_check; then
-        R $rargs CMD check -o /tmp isfs_[0-9].[0-9]-[0-9].tar.gz || exit $?
-        # R --vanilla --environ CMD check --use-valgrind -o /tmp eolts_*.tar.gz || exit $?
+        R $rargs CMD check -o /tmp isfs_[0-9].[0-9]*.tar.gz || exit $?
+        # R --vanilla --environ CMD check --use-valgrind -o /tmp isfs_*.tar.gz || exit $?
     fi
 fi
 
 if $do_install; then
-    scp eolts_0.0-1.tar.gz isfs_0.0-1.tar.gz porter2:/net/www/docs/software/R/src/contrib
+    scp eolts_[0-9]*.[0-9]*-*.tar.gz isfs_[0-9]*.[0-9]*-*.tar.gz porter2:/net/www/docs/software/R/src/contrib
 fi
 
