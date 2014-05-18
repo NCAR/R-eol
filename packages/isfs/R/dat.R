6bc722b25ae13b41f0f4673012f3a0bfbfc093e9
Gordon Maclean <maclean@users.noreply.github.com>
2014-05-18 17:28:19 -0600

    Rework of clip logic. Store clip check in logical vector, not an nts.

--- packages/isfs/R/dat.R
+++ packages/isfs/R/dat.R
@@ -434,14 +434,14 @@ setMethod("clip",signature(x1="dat"),
     function(x1,...)
     {
         for (i in 1:ncol(x1)) {
-            cn <- dimnames(x1[,i])[[2]]
+            cn <- colnames(x1[,i])
             if (!is.null(cl <- clip(cn)) && (is.null(cl$clip) || cl$clip)) {
                 cmin <- cl$min
                 cmax <- cl$max
-                clip.x <- x1[,i] < cmin & !is.na(x1[,i])
-                if (any(clip.x)) x1[clip.x,i] <- NA_real_
-                clip.x <- x1[,i] > cmax & !is.na(x1[,i])
-                if (any(clip.x)) x1[clip.x,i] <- NA_real_
+                clip.x <- x1@data[,i] < cmin & !is.na(x1@data[,i])
+                if (any(clip.x)) x1@data[clip.x,i] <- NA_real_
+                clip.x <- x1@data[,i] > cmax & !is.na(x1@data[,i])
+                if (any(clip.x)) x1@data[clip.x,i] <- NA_real_
             }
         }
         x1
