2e8d14c627288af09289b045bb82c44757fc11bd
Gordon Maclean <maclean@users.noreply.github.com>
2014-05-19 17:12:40 -0600

    fix function name

--- packages/eolsdng/R/plotScontour.R
+++ packages/eolsdng/R/plotScontour.R
@@ -7,7 +7,7 @@
 # The license and distribution terms for this file may be found in the
 # file LICENSE in this package.
 
-plotSContour <- function(xs,xname,yname,contour=FALSE)
+plotScontour <- function(xs,xname,yname,contour=FALSE)
 {
     xs <- interpSounding(xs,xname,yname)
 
