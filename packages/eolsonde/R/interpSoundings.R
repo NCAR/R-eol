4132ef9ce7cff9d7396bb9437f7083868a66d86a
Gordon Maclean <maclean@users.noreply.github.com>
2014-05-26 18:36:46 -0600

    Fixed some issues in interpSoundings. This is a working version. Now the task is to make it faster.

--- packages/eolsonde/R/interpSoundings.R
+++ packages/eolsonde/R/interpSoundings.R
@@ -13,10 +13,21 @@ interpSoundings <- function(sdngs, xname, ynames, xlim=NULL, nstep=100)
     xout <- NULL
     xunits <- NULL
     xreversed <- NULL
+    xm1 <- NULL
+    xm2 <- NULL
+
+    xmin <- Inf
+    xmax <- -Inf
 
     if (!is.list(sdngs))
         sdngs <- list(sdng=sdngs)
 
+    # First scan soundings to determine:
+    #   xreversed: whether xin is increasing or decreasing.
+    #       xout will be created with the same trend.
+    #   xunits: determine units of x variable and warn if they
+    #       are not consistent
+    #   xmin,xmax: which are used to create xout if xlim is NULL
     for (sname in names(sdngs)) {
 
         sdng <- sdngs[[sname]]
@@ -24,47 +35,89 @@ interpSoundings <- function(sdngs, xname, ynames, xlim=NULL, nstep=100)
         vnames <- colnames(sdng)
         if (is.na(match(xname,vnames))) stop(paste(xname,"not found in",sname))
 
-        xin <- as.vector(sdng@data[,xname])
-
-        xok <- !is.na(xin) & !is.infinite(xin)
+        xin <- clip(sdng[,xname])
+        xtunits <- eolts::units(xin)
+        xok <- !is.na(xin@data[,1]) & !is.infinite(xin@data[,1])
         if (sum(xok) < 10) next
-        xin <- xin[xok]
+        xin <- xin@data[xok,]   # no longer a time series
 
-        # compute means of first and last 10% of xin, and
-        # compare, in order to guess whether xin is increasing or decreasing
+        xmin <- min(xmin,xin)
+        xmax <- max(xmax,xin)
+
         nm <- length(xin) / 10
         if (nm < 2) nm <- length(xin) / 2
-
         m1 <- mean(xin[1:nm],trim=0.2)
         m2 <- mean(tail(xin,nm),trim=0.2)
+        xtreversed <- m1 > m2
 
-        if (is.null(xout)) {
-            xunits <- eolts::units(sdng[,xname])
-            xreversed <- m1 > m2
-            if (!is.null(xlim))
-                xout <- seq(from=xlim[1],to=xlim[2],length=nstep)
-            if (xunits == "mb") {
-                if (is.null(xlim)) xout <- seq(5,1200,length=nstep)
-                xiout <- log(xout)
-            }
-            else {
-                if (is.null(xlim)) xout <- seq(0,30000,length=nstep)
-                xiout <- xout
-            }
-            if (xreversed) {
-                # make xout have same upward/downward trend as xin
-                xout <- rev(xout)
-                xiout <- rev(xiout)
+        if (abs(m1-m2) > 1.e-4) {
+            if (is.null(xreversed)) {
+                xreversed <- xtreversed
+                xm1 <- m1
+                xm2 <- m2
+            } else {
+                if (xreversed != xtreversed) {
+                    warning(paste0("Trend of ",xname,
+                        " seems to be changing over these soundings. ",
+                        sname,": mean(first 10%)=",signif(m1,4),
+                        ", mean(last 10%)=",signif(m2,4),
+                        ", previous=",signif(xm1,4),", ",signif(xm2,4)))
+                    # If this difference of means is larger, use it.
+                    if (abs(m1-m2) > abs(xm1 - xm2)) {
+                        xreversed <- xtreversed
+                        xm1 <- m1
+                        xm2 <- m2
+                    }
+                }
             }
         }
         else {
-            xtunits <- eolts::units(sdng[,xname])
-            if (xunits != xtunits)
-                stop(paste("units of",xname,"are changing,",xunits,"!=",xtunits))
-            xtreversed <- m1 > m2
-            if (xreversed != xtreversed)
-                stop(paste("upward/downward trend of",xname,"seems to be changing over these soundings"))
+            warning(paste0(xname," in ",sname," appears to be constant.",
+                " mean(first 10%)=",signif(m1,4),
+                ", mean(last 10%)=",signif(m2,4),
+                ", stddev=",signif(sd(xin),4)))
         }
+        if (is.null(xunits)) {
+            xunits <- xtunits
+        } else if (xunits != xtunits) {
+            warning(paste("units of",xname,"are changing:",xunits,"!=",xtunits))
+        }
+    }
+    if (is.null(xlim)) {
+        if (is.infinite(xmin)) {
+            warning(paste("No data found for",xname,"in any input sounding"))
+            return(res) # empty list
+        }
+        xlim <- c(xmin,xmax)
+    }
+    else xlim <- sort(xlim)
+
+    xout <- seq(from=xlim[1],to=xlim[2],length=nstep)
+
+    # interpolate over log of pressure
+    if (xunits == "mb") xiout <- log(xout)
+    else xiout <- xout
+
+    if (xreversed) {
+        # make xout have same upward/downward trend as xin
+        xout <- rev(xout)
+        xiout <- rev(xiout)
+    }
+
+    for (sname in names(sdngs)) {
+
+        sdng <- sdngs[[sname]]
+
+        vnames <- colnames(sdng)
+
+        xin <- sdng[,xname]
+
+        xok <- !is.na(xin@data[,1]) & !is.infinite(xin@data[,1])
+        if (sum(xok) < 5) next
+        xin <- xin[xok,]
+
+        tin <- positions(xin)
+        dt <- deltat(xin)["trimmed.mean"]
 
         # interpolate over log of pressure
         if (xunits == "mb") xin <- log(xin)
@@ -78,16 +131,29 @@ interpSoundings <- function(sdngs, xname, ynames, xlim=NULL, nstep=100)
 
             if (is.na(match(yname,vnames))) stop(paste(yname,"not found in",sname))
 
-            yin <- clip(sdng[xok,yname])@data[,1]
+            # first interpolate yin to same times as xin
+            yin <- clip(sdng[,yname])
+            yin <- align(yin,tin,how="interp",matchtol=dt*10)
 
-            if (sum(!is.na(yin)) > 1)
-                rmat[,yname] <- approx(xin,yin,xiout)$y
+            if (sum(!is.na(yin)) > 2)
+                tryCatch(
+                    rmat[,yname] <- approx(xin@data[,1],yin@data[,1],xiout)$y,
+                    error=function(e)
+                    {
+                        # msg <- paste(e)
+                        # don't print out error message:
+                        # "need at least two non-NA values to interpolate"
+                        # User has been warned above in check of means
+                        if (all(!grepl("need at least two",format(e),fixed=TRUE)))
+                            warning(paste(format(e),collapse=": "),": ",sname,", ",xname,call.=FALSE)
+                        NA_real_
+                    }
+                )
             NULL
         }
 
-        tin <- as.numeric(positions(sdng))[xok]
-        t0  <- tin[1]
-        tn  <- tail(tin,1)
+        t0  <- as.numeric(tin[1])
+        tn  <- as.numeric(tail(tin,1))
         tx <- utime(t0 + seq(from=0,to=tn-t0,length.out=length(xout)))
 
         yunits <- eolts::units(sdng[,ynames])
