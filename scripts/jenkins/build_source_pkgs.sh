cce012ec4a769ad73b724e2a148fdac437baaf88
Gordon Maclean <maclean@users.noreply.github.com>
2015-10-22 22:07:48 -0600

    Set version from git tag

--- scripts/jenkins/build_source_pkgs.sh
+++ scripts/jenkins/build_source_pkgs.sh
@@ -5,6 +5,17 @@
 # revision will be the commit counts on the HEAD
 revision=$(git rev-list HEAD | wc -l)
 
+
+if ! gitdesc=$(git describe --match "v[0-9]*"); then
+    echo "git describe failed, looking for a tag vX.Y"
+    exit 1
+fi
+gitdesc=${gitdesc/#v}       # 2.0-14-gabcdef123
+version=${gitdesc%-*}       # 2.0-14
+
+[ $gitdesc == "$version" ] && version=${gitdesc}-0
+
+
 rargs="--vanilla"
 
 export R_LIBS_USER=$(mktemp -d $WORKSPACE/Rtemp_XXXXXX)
@@ -17,7 +28,7 @@ rm -f *.tar.gz
 # build eolts first. isfs and eolsonde depend on it.
 for pkg in eolts isfs eolsonde; do
 
-    sed -ri "s/^Version: *([0-9]+)\.([0-9]+)-.*/Version: \1.\2-$revision/" $pkg/DESCRIPTION
+    sed -ri "s/^Version:.*/Version: $version/" $pkg/DESCRIPTION
 
     cd $pkg; autoreconf; cd -
 
