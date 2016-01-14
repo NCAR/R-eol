b8f9743d8010601710d7e37af4f0dd4cf9ae293f
Gordon Maclean <maclean@users.noreply.github.com>
2016-01-13 19:19:44 -0700

    Added jenkins script

---
+++ scripts/jenkins/install_packages_from_repo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Build R packages from source tar files on the EOL repo
+# install to the first site library, .Library.site[1].
+
+pkgs="c('eolts','isfs','eolsonde')"
+
+rlib=$(R --vanilla --slave -e 'cat(.Library.site[1])')
+
+if [ ! -d $rlib ]; then
+    echo "$rlib not found"
+    exit 1
+fi
+
+R --vanilla -e "tryCatch(install.packages(pkgs=$pkgs,type='source',repos='https://www.eol.ucar.edu/software/R',lib='$rlib'),error=function(e)q(status=1))"
+
