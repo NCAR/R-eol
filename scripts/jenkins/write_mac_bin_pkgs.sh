30c958db55e4af24fbabe43badcc0b75cfc61872
Gordon Maclean <maclean@users.noreply.github.com>
2017-01-05 13:56:18 -0700

    Update how files are removed from repo

--- scripts/jenkins/write_mac_bin_pkgs.sh
+++ scripts/jenkins/write_mac_bin_pkgs.sh
@@ -10,11 +10,13 @@ repo=${R_REPO:?}/$(<contrib_path.txt)
 
 [ -d $repo ] || mkdir -p $repo
 
-rm -f $repo/eolts_*.tgz $repo/isfs_*.tgz $repo/eolsonde_*.tgz
-
 rsync -a *.tgz $repo
 
 rm -f *.tgz
+
+for pkg in eolts isfs eolsonde; do
+    rm -f $(ls $repo/${pkg}_*.tgz | sort | head -n -2)
+done
 
 R --vanilla -e "tryCatch(tools::write_PACKAGES(dir='$repo',type='mac.binary'),error=function(e)q(status=1))"
 
--- scripts/jenkins/write_src_pkgs.sh
+++ scripts/jenkins/write_src_pkgs.sh
@@ -7,24 +7,11 @@ repo=${R_REPO:?}/src/contrib
 
 [ -d $repo ] || mkdir -p $repo || exit 1
 
-tmpfile1=$(mktemp /tmp/$(basename $0)_XXXXXX)
-tmpfile2=$(mktemp /tmp/$(basename $0)_XXXXXX)
-trap "{ rm -f $tmpfile1 $tmpfile2; }" EXIT
-
-ls $repo/eolts_*.tar.gz $repo/isfs_*.tar.gz $repo/eolsonde_*.tar.gz | sort > $tmpfile1
-
 rsync -a *.tar.gz $repo
+
+for pkg in eolts isfs eolsonde; do
+    rm -f $(ls $repo/${pkg}_*.tar.gz | sort | head -n -2)
+done
 
 R --vanilla -e "tryCatch(tools::write_PACKAGES(dir='$repo',type='source'),error=function(e)q(status=1))"
 
-ls $repo/eolts_*.tar.gz $repo/isfs_*.tar.gz $repo/eolsonde_*.tar.gz | sort > $tmpfile2
-
-# number of new files on $repo
-nnew=$(comm -13 $tmpfile1 $tmpfile2 | wc -l)
-
-# If there are new files delete old ones.
-# The idea is not to delete a package until it is no
-# longer referenced in the PACKAGES file on the repository
-if [ $nnew -ge 2 ]; then
-    rm -f $(comm -2 $tmpfile1 $tmpfile2)
-fi
