d4e6710838ab69e377bb62a10fa0661736724bbb
Gordon Maclean <maclean@users.noreply.github.com>
2017-01-05 15:00:19 -0700

    More script tweaks

--- scripts/jenkins/write_mac_bin_pkgs.sh
+++ scripts/jenkins/write_mac_bin_pkgs.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
-
-umask 002
-
 # From working directory, copy .tgz mac.binary R packages to a
 # local R package repository
+
+umask 0002
 
 # contrib_path.txt was written by mac_build.sh, 
 repo=${R_REPO:?}/$(<contrib_path.txt)
 
-[ -d $repo ] || mkdir -p $repo
+if ! [ -d $repo ]; then
+    mkdir -p $repo || exit 1
+    chmod g+ws $repo
+fi
 
 rsync -a *.tgz $repo
-
 rm -f *.tgz
+
+R --vanilla -e "tryCatch(tools::write_PACKAGES(dir='$repo',type='mac.binary'),error=function(e)q(status=1))"
 
 for pkg in eolts isfs eolsonde; do
     rm -f $(ls $repo/${pkg}_*.tgz | sort | head -n -2)
     chmod g+w $repo/${pkg}_*.tgz
 done
-
-R --vanilla -e "tryCatch(tools::write_PACKAGES(dir='$repo',type='mac.binary'),error=function(e)q(status=1))"
-
-
--- scripts/jenkins/write_src_pkgs.sh
+++ scripts/jenkins/write_src_pkgs.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-
 # From working directory, copy source R packages to a
 # local R package repository
 
+umask 0002
+
 repo=${R_REPO:?}/src/contrib
 
-[ -d $repo ] || mkdir -p $repo || exit 1
+if ! [ -d $repo ]; then
+    mkdir -p $repo || exit 1
+    chmod g+ws $repo
+fi
 
 rsync -a *.tar.gz $repo
+
+R --vanilla -e "tryCatch(tools::write_PACKAGES(dir='$repo',type='source'),error=function(e)q(status=1))"
 
 for pkg in eolts isfs eolsonde; do
     rm -f $(ls $repo/${pkg}_*.tar.gz | sort | head -n -2)
     chmod g+w $repo/${pkg}_*.tar.gz
 done
-
-R --vanilla -e "tryCatch(tools::write_PACKAGES(dir='$repo',type='source'),error=function(e)q(status=1))"
 
