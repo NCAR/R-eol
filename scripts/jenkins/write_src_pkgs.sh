aad4b7332aa172c118a0c50af16263768fed40a3
Gordon Maclean <maclean@users.noreply.github.com>
2017-01-05 14:10:36 -0700

    Add group write

--- scripts/jenkins/write_src_pkgs.sh
+++ scripts/jenkins/write_src_pkgs.sh
@@ -11,6 +11,7 @@ rsync -a *.tar.gz $repo
 
 for pkg in eolts isfs eolsonde; do
     rm -f $(ls $repo/${pkg}_*.tar.gz | sort | head -n -2)
+    chmod g+w $repo/${pkg}_*.tar.gz
 done
 
 R --vanilla -e "tryCatch(tools::write_PACKAGES(dir='$repo',type='source'),error=function(e)q(status=1))"
