16027d57b8bbd1d2466f74bb9cd36a8079c16e0e
Gordon Maclean <maclean@users.noreply.github.com>
2018-06-27 22:22:15 -0600

    Rework of conform, with tests
    Fix and test conforming across sites and heights.

--- packages/isfs/R/conform.R
+++ packages/isfs/R/conform.R
@@ -14,30 +14,31 @@ setMethod("conform",signature(x="dat",y="dat"),
     function(x,y)
     {
 
-        xcols <- match_columns(y,x,warn="Creating time series, filled by")
+        xcols <- match_columns(y,x,warn=TRUE)
         if (any(xbad <- (xcols == 0))) {
+            warning(paste("columns",paste((1:length(xbad))[xbad], collapse=","),
+                    "will be filled with NA"))
             xcols[xbad] <- 1
+            # units will be the same as column 1
             x <- x[,xcols]
             x[,xbad] <- NA_real_
             xstns <- stations(x)
             xstns[xbad] <- stations(y)[xbad]
             stations(x) <- xstns
-
-            # what dimnames to we give the new columns?
-            # We'll give the NA columns a name made up of the
-            # first word of column 1 of x, and the suffixes of y
-            # Jeesh this gets complicated, and probably hides too much
-            # from the user.
-
-            dx <- dimnames(x)[[2]]
-            dy <- dimnames(y)[[2]]
-            dx[xbad] <- paste(words(dx[1],1,1),dy[xbad],sep=".")
-            dimnames(x)[[2]] <- dx
-
-            # units will also come from column 1
         }
-        else x <- x[,xcols]
+        else {
+            x <- x[,xcols]
+        }
+        cx <- paste0(words(colnames(x),1,1),suffixes(colnames(y)))
 
+        # fill in missing site names from y
+        sx <- sites(cx)
+        ns <- sx == ""
+        if (any(ns)) {
+            sx[ns] <- sites(x[,ns])
+            sites(cx) <- sx
+        }
+        colnames(x) <- cx
         x
     }
     )
@@ -69,16 +70,16 @@ setMethod("conform",signature(x="ANY",y="ANY"),
     }
     )
 
-setGeneric("match_columns",function(y,x,warn)
+setGeneric("match_columns",function(x,y,warn)
     standardGeneric("match_columns"))
 
-setMethod("match_columns",signature(y="dat",x="dat"),
-    function(y,x,warn=FALSE)
+setMethod("match_columns",signature(x="dat",y="dat"),
+    function(x,y,warn=FALSE)
     {
 
-        # for each column in y, its corresponding column in x.
-        # return 0 for those columns in y that don't
-        # correspond to any in x.
+        # for each column in x, its corresponding column in y.
+        # return 0 for those columns in x that don't
+        # correspond to any in y.
         # Match first by station number, then height, then suffix
         ncx <- ncol(x)
         ncy <- ncol(y)
@@ -91,19 +92,13 @@ setMethod("match_columns",signature(y="dat",x="dat"),
         if (length(stnsy) != ncy)
             stop("length(stations(y))=",length(stnsy)," is not equal to ncol(y)=",ncy)
 
-        dnx <- dimnames(x)[[2]]
-        dny <- dimnames(y)[[2]]
-        # cat("dnx=",paste(dnx,collapse=","),"\n")
+        dnx <- colnames(x)
+        dny <- colnames(y)
         # cat("dny=",paste(dny,collapse=","),"\n")
-
-        # if ((ncx == 1 && ncy == 1) ||
-        # 	(ncx == ncy && all(stnsx == stnsy))) return(1:ncy)
-
-        sfxsx <- words(dnx,2,sep=".")
-        sfxsy <- words(dny,2,sep=".")
+        # cat("dnx=",paste(dnx,collapse=","),"\n")
 
         htsx <- heights(x)
-        # Remove names so that "identical(htsx[ssx], NA_real_)" works
+        # Remove names so that "identical(htsy[ssy], NA_real_)" works
         names(htsx) <- NULL
         htsy <- heights(y)
         names(htsy) <- NULL
@@ -113,124 +108,122 @@ setMethod("match_columns",signature(y="dat",x="dat"),
         # cat("sitesx=",paste(sitesx,collapse=","),"\n")
         # cat("sitesy=",paste(sitesy,collapse=","),"\n")
 
-        # columns to grab from x in order to match them with y
+        # columns to grab from y in order to match them with x
         # 0 means no match
-        xcols <- rep(0,ncy)
+        ycols <- rep(0,ncx)
 
-        for (stn in unique(stnsy)) {
+        for (stn in unique(stnsx)) {
             stnx <- (1:ncx)[!is.na(match(stnsx,stn))]	# x column numbers of stn
             stny <- (1:ncy)[!is.na(match(stnsy,stn))]	# y column numbers of stn
 
             # check site names
-            sitey = sitesy[stny]
-            if (all(is.na(match(sitesx[stnx],sitey)))) sitey = ""
+            sitex <- sitesx[stnx]
+            # if (all(is.na(match(sitex, sitesy[stny])))) sitex <- ""
 
-            for (sitei in unique(sitey)) {
+            for (sitei in unique(sitex)) {
                 # cat("sitei=",sitei,"\n")
-                if (identical(sitey,"")) {
-                    ssx <- stnx
-                    ssy <- stny
-                }
-                else {
-                    # cat("stnx=",paste(stnx,collapse=","),"\n")
-                    # cat("stny=",paste(stnx,collapse=","),"\n")
-                    ssx <- stnx[!is.na(match(sitesx[stnx],sitei))]	# x column numbers of stn and site
-                    ssy <- stny[!is.na(match(sitesy[stny],sitei))]	# y column numbers of stn and site
-                    # cat("ssx=",paste(ssx,collapse=","),"\n")
-                    # cat("ssy=",paste(ssy,collapse=","),"\n")
-                    if (length(ssx) == 0) ssx = stnx
-                    if (length(ssy) == 0) ssy = stny
+                # cat("stnx=",paste(stnx,collapse=","),"\n")
+                # cat("stny=",paste(stny,collapse=","),"\n")
+                # x column numbers of stn and site
+                ssx <- stnx[!is.na(match(sitesx[stnx],sitei))]
 
-                }
+                # y column numbers matching station and site of x
+                ssy <- stny[!is.na(match(sitesy[stny],sitei))]
+                # cat("ssx=",paste(ssx,collapse=","),"\n")
+                # cat("ssy=",paste(ssy,collapse=","),"\n")
+                if (length(ssy) == 0 && sitei == "" ) ssy <- stny
+                if (length(ssy) == 0) ssy <- stny[sitesy[stny] == ""]
+
                 lssx <- length(ssx)
                 lssy <- length(ssy)
-                if (lssx == 0) {
-                    # no match for stn in x
+                if (lssy == 0) {
+                    # no match for stn in y
                     if ((is.logical(warn) && warn) || is.character(warn)) {
-                        warning(paste("No data in x (dimnames=",paste(dnx,collapse=","),
-                                ", stations=",paste(stnsx,collapse=","),") for station",stn))
+                        warning(paste0("No data in colnames=",paste(dny,collapse=","),
+                                ", stations=",paste(stnsy,collapse=",")," for station",stn))
                     }
                 }
                 else {
                     # conform along the heights
-                    for (hty in unique(htsy[ssy])) {
-                        # cat("hty=",hty,"\n")
-                        # hx is vector of x column numbers of hty and stn
-                        # hy is vector of y column numbers of hty and stn
+                    for (htx in unique(htsx[ssx])) {
+                        # cat("htx=",htx,"\n")
+                        # hx is vector of x column numbers of htx and stn
+                        # hy is vector of y column numbers of htx and stn
+                        hx <- (1:ncx)[ssx][!is.na(match(htsx[ssx],htx))]
+                        lhx <- length(hx)
 
-                        # If x at this point has one column, with NA height, it matches against anyting
-                        if (identical(htsx[ssx], NA_real_)) {
+                        # If y at this point has one column, with NA height, it matches against anything
+                        if (identical(htsy[ssy], NA_real_)) {
                             # one column, no height information
-                            hx = (1:ncx)[ssx]
+                            hy = (1:ncy)[ssy]
                         }
                         else {
-                            hx <- (1:ncx)[ssx][!is.na(match(htsx[ssx],hty))]
+                            # matching heights
+                            hy <- (1:ncy)[ssy][!is.na(match(htsy[ssy],htx))]
                         }
-                        lhx <- length(hx)
-                        hy <- (1:ncy)[ssy][!is.na(match(htsy[ssy],hty))]
                         lhy <- length(hy)
-                        if (lhx == 0 && !is.na(hty)) {
+                        if (lhy == 0 && !is.na(htx)) {
                             # no exact match for this height at stn
                             # find minimum difference
-                            hd = abs(htsx[ssx]-hty)
+                            hd = abs(htsy[ssy]-htx)
                             hdm = min(hd,na.rm=T)
                             # cat("height differences=",hd," minimum=",hdm,"\n")
                             if (!is.na(hdm)) {
-                                hx <- (1:ncx)[ssx][!is.na(hd) & hd == hdm]
-                                lhx <- length(hx)
-                                if (length(lhx) > 0 && ((is.logical(warn) && warn) || is.character(warn))) {
-                                    warning(paste("conforming",dnx[hx],", (stns=",stnsx[hx],") to",
-                                            paste(dny[hy],"(stn=",stnsy[hy],")",sep="",collapse=", "),
+                                hy <- (1:ncy)[ssy][!is.na(hd) & hd == hdm]
+                                lhy <- length(hy)
+                                if (length(lhy) > 0 && ((is.logical(warn) && warn) || is.character(warn))) {
+                                    warning(paste("conforming",dny[hy],", (stns=",stnsy[hy],") to",
+                                            paste(dnx[hx],"(stn=",stnsx[hx],")",sep="",collapse=", "),
                                             "(matching minimum height difference)"))
                                 }
                             }
                             else {
-                                # all height differences are NA, but hty
-                                # is not NA. Therefore all heights of x are NA
-                                hx <- (1:ncx)[ssx][is.na(htsx[ssx])]
-                                lhx <- length(hx)
-                                if (length(lhx) > 0 && ((is.logical(warn) && warn) || is.character(warn))) {
-                                    warning(paste("conforming",dnx[hx],", (stns=",stnsx[hx],") to",
-                                            paste(dny[hy],"(stn=",stnsy[hy],")",sep="",collapse=", "),
-                                            "(matching NA heights in x to non-NA heights in y)"))
+                                # all height differences are NA, but htx
+                                # is not NA. Therefore all heights of y are NA
+                                hy <- (1:ncy)[ssy][is.na(htsy[ssy])]
+                                lhy <- length(hy)
+                                if (length(lhy) > 0 && ((is.logical(warn) && warn) || is.character(warn))) {
+                                    warning(paste("conforming",dny[hy],", (stns=",stnsy[hy],") to",
+                                            paste(dnx[hx],"(stn=",stnsx[hx],")",sep="",collapse=", "),
+                                            "(matching NA heights in y to non-NA heights in x)"))
                                 }
                             }
                         }
 
-                        if (lhx == 1) {
-                            # one x column for this station & height, repeat it
-                            xcols[hy] <- rep(hx,lhy)
+                        if (lhy == 1) {
+                            # one y column for this station & height, repeat it
+                            ycols[hx] <- rep(hy,lhx)
                         }
-                        else if (lhx == 0) {
+                        else if (lhy == 0) {
                             if ((is.logical(warn) && warn) || is.character(warn)) {
-                                warning(paste("No data in x (dimnames=",paste(dnx,collapse=","),
-                                        ", stns=",paste(stnsx,collapse=","),") for station",stn,
-                                        "at height",hty))
+                                warning(paste0("No data in colnames=",paste(dny,collapse=","),
+                                        ", stns=",paste(stnsy,collapse=",")," for station",stn,
+                                        "at height",htx))
                             }
                         }
                         else {
                             # if same number of columns with same height at a station,
                             # set default conformation
 
-                            # more than one column of x. Might be something like CHATS:
-                            #   x=Vazimuth.1b.9.6m.ha Vazimuth.2b.9.6m.ha Vazimuth.3b.9.6m.ha ...
-                            #   y=u.1b.9.6m.ha u.2b.9.6m.ha u.3b.9.6m.ha u.4b.9.6m.ha ...
-                            if (lhx == lhy) {
+                            # more than one column of y. Might be something like CHATS:
+                            #   y=Vazimuth.1b.9.6m.ha Vazimuth.2b.9.6m.ha Vazimuth.3b.9.6m.ha ...
+                            #   x=u.1b.9.6m.ha u.2b.9.6m.ha u.3b.9.6m.ha u.4b.9.6m.ha ...
+                            if (lhy == lhx) {
                                 # try match of names after first word
-                                my = match(words(dny[hy],2),words(dnx[hx],2),nomatch=0)
-                                if (any(my == 0)) xcols[hy] <- hx
-                                else xcols[hy] = hx[my]
+                                mx = match(words(dnx[hx],2),words(dny[hy],2),nomatch=0)
+                                if (any(mx == 0)) ycols[hx] <- hy
+                                else ycols[hx] = hy[mx]
                             }
                             else {
                                 msg <- paste("more than one column of",
-                                    paste(dnx[hx],"(stns=",stnsx[hx],")",sep="",collapse=", "),
+                                    paste(dny[hy],"(stns=",stnsy[hy],")",sep="",collapse=", "),
                                     "conforms to",
-                                    paste(dny[hy],"(stns=",stnsy[hy],")",sep="",collapse=", "))
-                                tx <- hy[xcols[hy] == 0]
+                                    paste(dnx[hx],"(stns=",stnsx[hx],")",sep="",collapse=", "))
+                                ty <- hx[ycols[hx] == 0]
                                 msg <- paste(msg,"Will repeat",
-                                    dnx[hx][1],"(stn=",stnsx[hx][1],")",
-                                    length(tx),"times")
-                                xcols[tx] <- rep(hx[1],length(tx))
+                                    dny[hy][1],"(stn=",stnsy[hy][1],")",
+                                    length(ty),"times")
+                                ycols[ty] <- rep(hy[1],length(ty))
                                 if ((is.logical(warn) && warn) || is.character(warn))
                                     warning(msg)
                             }
@@ -239,23 +232,22 @@ setMethod("match_columns",signature(y="dat",x="dat"),
                 }
             }
         }
-        if (any(xcols == 0)) {
+        if (any(ycols == 0) && ((is.logical(warn) && warn) || is.character(warn))) {
             if (is.character(warn)) warnstr <- warn
-            else warnstr <- ""
-            tx <- (1:ncy)[xcols == 0]
+            warnstr <- ""
+            ty <- (1:ncx)[ycols == 0]
             msg <- paste("Cannot conform",
-                paste(dnx,"(stn=",stnsx,")",sep="",collapse=", "),
+                paste(dny,"(stn=",stnsy,")",sep="",collapse=", "),
                 "to",
-                paste(dny[tx],"(stn=",stnsy[tx],")",sep="",collapse=", "),
+                paste(dnx[ty],"(stn=",stnsx[ty],")",sep="",collapse=", "),
                 ".",warnstr)
-            if ((is.logical(warn) && warn) || is.character(warn))
-                warning(msg)
+            warning(msg)
         }
-        # cat(paste("match_columns of",paste(dny,collapse=","),"to",
-        #         paste(dnx,collapse=","),"is",
-        #         paste(xcols,collapse=",")),"\n")
-        # if (any(is.na(xcols))) browser()
-        # xcols[is.na(xcols)] = 0
-        xcols
+        # cat(paste("match_columns of",paste(dnx,collapse=","),"to",
+        #         paste(dny,collapse=","),"is",
+        #         paste(ycols,collapse=",")),"\n")
+        # if (any(is.na(ycols))) browser()
+        # ycols[is.na(ycols)] = 0
+        ycols
     }
     )
---
+++ packages/isfs/inst/unitTests/runit.conform.R
@@ -0,0 +1,147 @@
+# -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4; -*-
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
+#
+# 2013,2014, Copyright University Corporation for Atmospheric Research
+# 
+# This file is part of the "eolts" package for the R software environment.
+# The license and distribution terms for this file may be found in the
+# file LICENSE in this package.
+
+test.conform <- function()
+{
+    nr <- 4
+    t0 <- utime("2018 Jun 21 00:00")
+    tx <- utime(seq(as.numeric(t0), length=nr, by=1))
+
+    # basic variable names with heights, no sites
+    ncx <- 3
+    cx <- paste0("P.", 1:ncx, "m")
+    ux <- rep("m", ncx)
+    x <- dat(nts(matrix(as.double(1:ncx), byrow=TRUE, nrow=nr, ncol=ncx), tx,
+            units=ux, names=cx))
+
+    ncy <- 5
+    cy <- paste0("T.", 1:ncy, "m")
+    uy <- rep("cm", ncy)
+    y <- dat(nts(matrix(as.double(1:ncy), byrow=TRUE, nrow=nr, ncol=ncy), tx,
+            units=uy, names=cy))
+
+    cxc <- paste0("P.", 1:ncy, "m")
+
+    cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y),collapse=","),"\n")
+    # all columns of x should conform to y
+    xx <- conform(x, y[,1:ncx])
+    # cat("colnames(xx)=",paste(colnames(xx),collapse=","),"\n")
+    checkIdentical(x, xx)
+
+    xx <- conform(x, y)
+    checkIdentical(dim(xx), dim(y))
+    checkIdentical(x, xx[,1:ncx])
+    checkTrue(all(sapply((ncx+1):ncy, function(i) { all(xx[,i] == x[,ncx]) } )))
+    # cat("units(xx)=",paste(units(xx),collapse=","),"\n")
+    checkIdentical(units(xx), rep(ux[1],ncy))
+
+    # conforming x to one column of y should return one column of x
+    for (ic in 1:ncx) {
+        cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y[,ic]),collapse=","),"\n")
+        # cat("ic=",ic,"\n")
+        xx <- conform(x, y[,ic])
+        checkIdentical(dim(xx), dim(y[,ic]))
+        checkIdentical(x[,ic], xx)
+    }
+
+    # conforming x to y at a higher height should return x at highest
+    for (ic in (ncx+1):ncy) {
+        cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y[,ic]),collapse=","),"\n")
+        # cat("ic=",ic,"\n")
+        xx <- conform(x, y[,ic])
+        checkIdentical(dim(xx), dim(y[,ic]))
+        # cat("colnames(xx)=",paste(colnames(xx),collapse=","),"\n")
+        checkTrue(all(xx[,1] == x[,ncx]))
+        checkIdentical(colnames(xx), cxc[ic])
+    }
+
+    # x has site name, y doesn't. This should conform.
+    # We're treating no site name like a wildcard, i.e. any site.
+    # cat("sites(x)=", paste0(sites(x),","),"\n")
+    colnames(x) <- paste0(cx, ".sx")
+    # cat("colnames(x)=", paste0(colnames(x),","),"\n")
+    cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y),collapse=","),"\n")
+    xx <- conform(x, y)
+    # cat("colnames(xx)=",paste(colnames(xx),collapse=","),"\n")
+    checkIdentical(x, xx[,1:ncx])
+    checkIdentical(colnames(xx),paste0(cxc, ".sx"))
+    checkTrue(all(sapply(1:ncx, function(i) { all(xx[,i] == i) } )))
+    checkTrue(all(sapply((ncx+1):ncy, function(i) { all(xx[,i] == x[,ncx]) } )))
+    # cat("units(xx)=", paste0(units(xx),collapse=","),"\n")
+    checkIdentical(units(xx), rep(ux[1],ncy))
+
+    colnames(x) <- cx
+
+    # y has site name, x doesn't. This should also conform.
+    colnames(y) <- paste0(cy, ".sy")
+    cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y),collapse=","),"\n")
+    xx <- conform(x, y)
+    # cat("colnames(xx)=",paste(colnames(xx),collapse=","),"\n")
+    checkIdentical(colnames(xx),paste0(cxc, ".sy"))
+    checkTrue(all(sapply(1:ncx, function(i) { all(xx[,i] == i) } )))
+    checkTrue(all(sapply((ncx+1):ncy, function(i) { all(xx[,i] == x[,ncx]) } )))
+    checkIdentical(units(xx), rep(ux[1],ncy))
+    colnames(y) <- cy
+
+    # Different site names. Should not conform.
+    colnames(x) <- paste0(cx, ".sx")
+    colnames(y) <- paste0(cy, ".sy")
+    cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y),collapse=","),"\n")
+    xx <- conform(x, y)
+    # cat("colnames(xx)=",paste(colnames(xx),collapse=","),"\n")
+    checkIdentical(paste0(cxc,".sy"), colnames(xx))
+    checkTrue(all(is.na(xx)))
+    checkIdentical(units(xx), rep(ux[1], ncy))
+
+    # conform x at heights and sites 1,2,3 to y at heights and sites 2,4.
+    # Should get two column result, consisting of x at 2 and NAs.
+    cxx <- paste0(cx,paste0(".s",1:ncx))
+    colnames(x) <- cxx
+    cxy <- paste0(cy,paste0(".s",1:ncy))
+    colnames(y) <- cxy
+    cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y[,c(2,4)]),collapse=","),"\n")
+    xx <- conform(x, y[,c(2,4)])
+    # cat("colnames(xx)=",paste(colnames(xx),collapse=","),"\n")
+    checkIdentical(dim(xx), dim(y[,c(2,4)]))
+    checkIdentical(colnames(xx), c(colnames(x[,2]),paste0(cxc[4],".s4")))
+    checkTrue(all(xx[,1] ==  2))
+    checkTrue(all(is.na(xx[,2])))
+
+    cx <- paste0("P.5m.s", 1:ncx)
+    colnames(x) <- cx
+    cy <- paste0("T.6m.s", 1:ncy)
+    colnames(y) <- cy
+    cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y),collapse=","),"\n")
+    xx <- conform(x, y)
+    # cat("colnames(xx)=",paste(colnames(xx),collapse=","),"\n")
+
+    checkIdentical(dim(xx), dim(y))
+    checkTrue(all(sapply(1:ncx, function(i) { all(xx[,i] == x[,i]) } )))
+    checkIdentical(colnames(xx), paste0(words(cx[1],1,1),suffixes(cy)))
+    checkTrue(all(is.na(xx[,(ncx+1):ncy])))
+    # cat("units(xx)=",paste(units(xx),collapse=","),"\n")
+    checkIdentical(units(xx), rep(ux[1],ncy))
+
+    cx <- paste0("Rpile.in.s",1:ncx)
+    colnames(x) <- cx
+    cy <- paste0("Tcase.in.s",1:ncy)
+    colnames(y) <- cy
+    cat("checking conform of",paste0(colnames(x),collapse=","),"to",paste0(colnames(y),collapse=","),"\n")
+    xx <- conform(x, y)
+    # browser()
+    # cat("colnames(xx)=",paste(colnames(xx),collapse=","),"\n")
+    checkIdentical(dim(xx), dim(y))
+    checkTrue(all(sapply(1:ncx, function(i) { all(xx[,i] == x[,i]) } )))
+    checkIdentical(x, xx[,1:ncx])
+    checkTrue(all(is.na(xx[,(ncx+1):ncy])))
+    # cat("units(xx)=",paste(units(xx),collapse=","),"\n")
+    checkIdentical(units(xx), rep(ux[1],ncy))
+
+    return()
+}
