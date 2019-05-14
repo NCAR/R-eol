3a67f8bbb07e66cc4cd42ae02fc4bd6a6cea1917
Gordon Maclean <maclean@users.noreply.github.com>
2019-05-13 18:26:25 -0600

    Fix removal of old packages

--- scripts/jenkins/write_src_pkgs.sh
+++ scripts/jenkins/write_src_pkgs.sh
@@ -16,7 +16,7 @@ rsync -a *.tar.gz $repo
 R --vanilla -e "tryCatch(tools::write_PACKAGES(dir='$repo',type='source'),error=function(e)q(status=1))"
 
 for pkg in eolts isfs eolsonde; do
-    rm -f $(ls $repo/${pkg}_*.tar.gz | sort | head -n -2)
+    rm -f $(ls -rt $repo/${pkg}_*.tar.gz | sort | head -n -2)
     chmod g+w $repo/${pkg}_*.tar.gz
 done
 
